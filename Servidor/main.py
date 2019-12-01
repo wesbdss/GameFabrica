692f92bfad9817a24235490210b79df3fd481c97
wesbdss <wesleybenicio4@gmail.com>
2019-12-01 15:42:08 -0300

    Heroku

--- Servidor/main.py
+++ Servidor/main.py
@@ -5,6 +5,7 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import socket
+import os
 sys.path.insert(1, './Servidores/')
 from webSocketPlay import SocketPlay
 from webService import WebHandle
@@ -27,14 +28,16 @@ estrutura json:
 
 """
 
+
 def main():
     application = tornado.web.Application([
         (r'/event', SocketPlay),(r'/', WebHandle)])
     
     
     if __name__ == "__main__":
+    	port = int(os.environ.get("PORT", 8080))
         http_server = tornado.httpserver.HTTPServer(application)
-        http_server.listen(8080)
+        http_server.listen(port)
         myIP = socket.gethostbyname(socket.gethostname())
         print ('*** Websocket Server Started at %s ***' % myIP)
         tornado.ioloop.IOLoop.instance().start()
